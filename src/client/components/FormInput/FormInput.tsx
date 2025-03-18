e42fd127585378178343ad36b0e309c7550c31d0
Abilon Dias <abilon.amd@gmail.com>
2025-03-18 20:40:45 +0200

    refactor: implement influencer form with react-hook-form

---
+++ src/client/components/FormInput/FormInput.tsx
@@ -0,0 +1,39 @@
+import { FC, InputHTMLAttributes } from "react"
+
+interface FieldError {
+  message?: string
+}
+
+type FormFieldErrorProps = {
+  error: FieldError | undefined
+}
+
+interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  error: FieldError | undefined
+}
+
+export const FormInput: FC<FormInputProps> = (props) => {
+  const { label, error, className, ...remaining } = props
+
+  return (
+    <div>
+      <label className="form-label">{label}</label>
+      <input
+        className={`form-control ${error ? "is-invalid" : ""} ${className}`}
+        type="text"
+        placeholder="John"
+        {...remaining}
+      />
+      <FormFieldError error={error} />
+    </div>
+  )
+}
+
+export const FormFieldError: FC<FormFieldErrorProps> = ({ error }) => {
+  if (!error) {
+    return
+  }
+
+  return <span className="invalid-feedback">{error.message}</span>
+}
--- src/client/routes/influencers/InfluencerForm.tsx
+++ src/client/routes/influencers/InfluencerForm.tsx
@@ -1,154 +1,110 @@
-import {
-  ChangeEvent,
-  FormEvent,
-  MouseEvent,
-  useCallback,
-  useState,
-} from "react"
-import { Account, SocialMedia } from "."
+import { MouseEvent, useState } from "react"
+import { Influencer, SocialMedia } from "."
 import { useLoaderData, useNavigate } from "react-router"
 import { useNotify } from "../../contexts/NotificationContext"
 import { FormError, TFormError } from "../../components/FormError"
+import { useForm, useFieldArray, SubmitHandler } from "react-hook-form"
+import { FormInput } from "../../components/FormInput"
+import { FormFieldError } from "../../components/FormInput/FormInput"
 
 const InfluencerForm = () => {
   const { socialMedias } = useLoaderData() as { socialMedias: SocialMedia[] }
+
   const navigate = useNavigate()
   const notify = useNotify()
+  const [formError, setFormError] = useState<TFormError>()
 
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [accounts, setAccounts] = useState<Account[]>([
-    { social_media_id: socialMedias[0].id, name: "" },
-  ])
-  const [error, setError] = useState<TFormError>()
-
-  const updateFirstName = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
-    ev.preventDefault()
-    setFirstName(ev.target.value)
-  }, [])
-
-  const updateLastName = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
-    ev.preventDefault()
-    setLastName(ev.target.value)
-  }, [])
-
-  const addAccount = useCallback((ev: FormEvent) => {
-    ev.preventDefault()
-
-    setAccounts((prevAccounts) => [
-      ...prevAccounts,
-      { social_media_id: socialMedias[0].id, name: "" },
-    ])
-  }, [])
-
-  const updateAccountField = useCallback(
-    (index: number, field: keyof Account) =>
-      (ev: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
-        ev.preventDefault()
-
-        setAccounts((prevAccounts) =>
-          prevAccounts.map((account, i) => {
-            if (i !== index) {
-              return account
-            }
-
-            if (field === "social_media_id") {
-              return {
-                ...account,
-                social_media_id: Number.parseInt(ev.target.value),
-              }
-            }
-
-            return { ...account, [field]: ev.target.value }
-          }),
-        )
-      },
-    [],
-  )
-
-  const removeAccount = useCallback(
-    (index: number) => (ev: MouseEvent) => {
-      ev.preventDefault()
-
-      if (accounts.length === 1) {
-        return
-      }
-
-      setAccounts((prevAccounts) => prevAccounts.filter((_, i) => i !== index))
+  const {
+    control,
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<Influencer>({
+    defaultValues: {
+      accounts: [{ name: "", social_media_id: socialMedias[0].id }],
     },
-    [accounts],
-  )
+  })
+  const accounts = useFieldArray({
+    name: "accounts",
+    control,
+  })
 
-  const handleForm = useCallback(
-    async (ev: React.FormEvent) => {
+  const addAccount = (ev: MouseEvent<HTMLButtonElement>) => {
+    ev.preventDefault()
+    clearErrors("accounts")
+    accounts.append({ social_media_id: 1, name: "" })
+  }
+
+  const removeAccount =
+    (index: number) => (ev: MouseEvent<HTMLButtonElement>) => {
       ev.preventDefault()
+      accounts.remove(index)
+    }
 
-      try {
-        const response = await fetch("/api/influencers", {
-          method: "post",
-          body: JSON.stringify({
-            first_name: firstName,
-            last_name: lastName,
-            accounts,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
+  const submit: SubmitHandler<Influencer> = async (data) => {
+    try {
+      const response = await fetch("/api/influencers", {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
 
-        const parsed = await response.json()
-        if (!response.ok) {
-          setError(parsed)
-          return
-        }
-
+      if (response.ok) {
         notify({
           message: "Influencer created.",
           level: "success",
         })
         navigate("/influencers")
-      } catch (error) {
-        setError({ message: "Unexpected request error", details: [] })
-        console.log(error)
+        return
       }
-    },
-    [firstName, lastName, accounts],
-  )
+
+      const parsed: TFormError = await response.json()
+      if (!parsed.message) {
+        setFormError({ message: "Unexpected API Error" })
+        return
+      }
+      if (!parsed.details || parsed.details.length === 0) {
+        setFormError({ message: parsed.message })
+        return
+      }
+
+      parsed.details.forEach(({ message, field }) =>
+        setError(field, { message }),
+      )
+    } catch (error) {
+      setFormError({ message: "Unexpected request error" })
+      console.log(error)
+    }
+  }
 
   return (
     <>
       <h2>New Influencer</h2>
 
-      <form onSubmit={handleForm}>
-        <FormError error={error} />
+      <form onSubmit={handleSubmit(submit)}>
+        <FormError error={formError} />
 
         <div className="row">
           <div className="col-md-6">
-            <label className="form-label" htmlFor="first_name">
-              First Name
-            </label>
-            <input
-              className="form-control"
+            <FormInput
+              label="First Name"
+              error={errors.first_name}
               type="text"
-              name="first_name"
-              id="first_name"
-              value={firstName}
-              onChange={updateFirstName}
               placeholder="John"
+              {...register("first_name", {})}
             />
           </div>
           <div className="col-md-6">
-            <label className="form-label" htmlFor="last_name">
-              Last Name
-            </label>
-            <input
-              className="form-control"
+            <FormInput
+              label="Last Name"
+              error={errors.last_name}
               type="text"
-              name="last_name"
-              id="last_name"
-              value={lastName}
-              onChange={updateLastName}
               placeholder="Doe"
+              {...register("last_name", {})}
             />
           </div>
         </div>
@@ -156,58 +112,60 @@ const InfluencerForm = () => {
         <div className="mt-2">
           <strong>Accounts</strong>
 
-          {accounts.map((account, accountIndex) => (
-            <div className="card mt-3" key={accountIndex}>
-              <div className="card-body pt-2 pb-2">
-                <div className="row">
-                  <div className="col-md-6">
-                    <p>
-                      <label className="form-label" htmlFor="social_media_id">
-                        Social Media
-                      </label>
-                      <select
-                        className="form-select"
-                        name="social_media_id"
-                        value={account.social_media_id}
-                        onChange={updateAccountField(
-                          accountIndex,
-                          "social_media_id",
-                        )}
-                      >
-                        {socialMedias.map((sm, i) => (
-                          <option value={sm.id} key={i}>
-                            {sm.title}
-                          </option>
-                        ))}
-                      </select>
-                    </p>
-                  </div>
-                  <div className="col-md-6">
-                    <label className="form-label">Account Name</label>
-                    <div className="input-group">
-                      <input
-                        className="form-control"
-                        type="text"
-                        value={account.name}
-                        onChange={updateAccountField(accountIndex, "name")}
-                        placeholder="johndoe_tt"
-                      />
-                      {accounts.length > 1 && (
-                        <button
-                          className="btn btn-danger"
-                          onClick={removeAccount(accountIndex)}
-                        >
-                          x
-                        </button>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          {accounts.fields.map((field, index) => {
+            let socialMediaIdError
+            let accountNameError
+            if (errors.accounts) {
+              socialMediaIdError = errors.accounts[index]?.social_media_id
+              accountNameError = errors.accounts[index]?.name
+            }
 
-          <p className="mt-2">
+            return (
+              <div className="row" key={field.id}>
+                <div className="col">
+                  <label className="form-label">Social Media</label>
+                  <select
+                    className={`form-select ${
+                      socialMediaIdError ? "is-invalid" : ""
+                    }`}
+                    {...register(`accounts.${index}.social_media_id`, {
+                      valueAsNumber: true,
+                    })}
+                  >
+                    {socialMedias.map((sm, i) => (
+                      <option value={sm.id} key={i}>
+                        {sm.title}
+                      </option>
+                    ))}
+                  </select>
+                  <FormFieldError error={socialMediaIdError} />
+                </div>
+                <div className="col-md-6">
+                  <FormInput
+                    label="Account Name"
+                    error={accountNameError}
+                    type="text"
+                    placeholder="johndoe_tt"
+                    {...register(`accounts.${index}.name`)}
+                  />
+                </div>
+                {accounts.fields.length > 1 && (
+                  <div className="col pt-4">
+                    <button
+                      className="btn btn-danger mt-2"
+                      onClick={removeAccount(index)}
+                    >
+                      ✕
+                    </button>
+                  </div>
+                )}
+              </div>
+            )
+          })}
+
+          <FormFieldError error={errors.accounts} />
+
+          <p className="mt-3">
             <button className="btn btn-primary" onClick={addAccount}>
               Add Account
             </button>
